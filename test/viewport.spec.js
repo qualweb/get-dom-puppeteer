29e43242f3dc6a700f5053b54ab088a83b8acd36
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-10-07 17:11:50 +0100

    added test to viewport attributes

--- test/viewport.spec.js
+++ test/viewport.spec.js
@@ -5,7 +5,7 @@ const stew = new(require('stew-select')).Stew();
 const URL = 'http://accessible-serv.lasige.di.fc.ul.pt/~jvicente/test/';
 
 describe('Viewport test', function() {
-  it.only('Should have the viewport attributes', async function() {
+  it('Should have the viewport attributes', async function() {
     this.timeout(10 * 1000);
     const dom = await getDom(URL);
 
